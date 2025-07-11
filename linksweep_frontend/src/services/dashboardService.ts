7f6d1fd5945820b7d6d28aca5d13a499a6360118
CareerServices-Pace <careers@pace.edu>
2025-07-11 00:50:54 -0400

    Updated dashboardService.ts

--- linksweep_frontend/src/services/dashboardService.ts
+++ linksweep_frontend/src/services/dashboardService.ts
@@ -1,7 +1,6 @@
 
 import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8000/';
+import { apiClient } from './Auth';
 
 // Create axios instance with base configuration
 const apiClient = axios.create({
